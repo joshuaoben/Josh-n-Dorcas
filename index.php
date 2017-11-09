d66321b8acfc5446f86f4acbc0be42e0f3489349
dorcas1 <dorcasoyebokun>
2017-11-09 13:49:51 +0100

    testing2

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 	echo "This is a quick project by Joshua and Dorcas";
 	echo "we are presently working on got conflict";
-	echo "Joshua is a boy and Dorcas loves to eat junks"
+	echo "Joshua is a boy and Dorcas loves to eat junks";
 	echo "Joshua just made a change";
+	echo "we want to test addiing and removing";
+
 ?>
